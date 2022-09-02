5fe5aa96912f674d66ec8801834fae2dda84c0a2
Zack Cheng <61947377+zackchengyk@users.noreply.github.com>
2022-09-02 13:57:55 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,67 +3,69 @@
 #include <array>
 #include <vector>
 #include <tuple>
+#include <numbers>
 
-// task 3, task 4: timesTwo() here
+// Task 3, Task 4: timesTwo() here
 
 struct Rectangle {
     double length; // data member, also known as a field
     double width = 1; // field can have a default value
-    // fields must be explicitly typed, you cannot use type deduction here
+    // Fields must be explicitly typed, you cannot use type deduction here
 
-    // member function, also known as a method
+    // Member function, also known as a method
     double calculateArea() {
         return length * width;
     }
 
-    // member function that modifies the state of an object
+    // Member function that modifies the state of an object
     void makeItASquare(double sideLength) {
         length = sideLength;
         width = sideLength;
     }
 
-    // task 5: calculatePerimeter() here
+    // Task 5: calculatePerimeter() here
 };
 
-// task 6: struct Circle here
+// Task 6: struct Circle here
 
-// task 7:
+// Task 7:
 void printShape(auto shape) {
     // Your code here
 }
 
-// task 9:
+// Task 9:
 void inplaceTimesTwo(/* ??? pointerToSomeVariable */) {
     // your code here
 }
 
-// task 10:
+// Task 10:
 void doubleEachElement(/* ??? container */) {
     // your code here
 }
 
 int main() {
-    // task2: write "Hello World!" in an std::cout << "" << std::endl; call
+    // Task 2: write "Hello World!" in an std::cout << "" << std::endl; call
 
-    // task3
+    // Task 3:
     // std::cout << timesTwo(21) << std::endl;
 
-    // task4
+    // Task 4:
     // std::cout << timesTwo(123) << std::endl;
     // std::cout << timesTwo(3.14) << std::endl;
     // std::cout << timesTwo(std::string{ "abc" }) << std::endl;
 
-    // task5
+    // Task 5:
     // std::cout << Rectangle{ 7, 8 }.calculatePerimeter() << std::endl;
 
-    // task6: create a few instances of Circle, and call their member functions
+    // Task 6: create a few instances of Circle, and call their member functions
+    // tip: to use pi, use std::numbers::pi
 
-    // task7: call printShape with different Rectangle and Circle objects
+    // Task 7: call printShape with different Rectangle and Circle objects
 
-    // task8: create a container of strings, fill the container with some strings
-    //        apply timesTwo to each string element in the container, print each string element in the container
+    // Task 8: create a container of strings, fill the container with some strings
+    //         apply timesTwo to each string element in the container, print each string element in the container
 
-    // task9
+    // Task 9:
     // auto x = 21;
     // auto y = std::string{ "abcd" };
     // inplaceTimesTwo(&x);
@@ -71,6 +73,6 @@ int main() {
     // std::cout << x << std::endl; // you should see 42 here
     // std::cout << y << std::endl; // you should see "abcdabcd" here
 
-    // task10: pass different std::array and std::vector objects to doubleEachElement
-    //        print the results after doubleEachElement calls
+    // Task 10: pass different std::array and std::vector objects to doubleEachElement
+    //          print the results after doubleEachElement calls
 }
